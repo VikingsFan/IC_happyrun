adac8a59befcdb2aa8649e9bf553c3243a060613
first-cxy <cscserer@sina.com>
2014-01-22 01:55:31 +0800

    test1

--- HappyRun/proj.win32/main.cpp
+++ HappyRun/proj.win32/main.cpp
@@ -15,7 +15,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
-    eglView->setViewName("Happy");
+    eglView->setViewName("Happy1");
     eglView->setFrameSize(480, 320);
     return CCApplication::sharedApplication()->run();
 }
